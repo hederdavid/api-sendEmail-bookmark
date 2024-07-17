10ec1a7f8ec3161090daa7f86fbbdbd80a103436
Heder David <hedermd6@gmail.com>
2024-07-17 12:38:48 -0300

    modificacoes de localhost

--- src/main/java/com/bookmark/email/CorsConfiguration.java
+++ src/main/java/com/bookmark/email/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://book-mark-landing-page-pearl.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*");;
     }
